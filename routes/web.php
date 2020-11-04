9bc43288357e056866d3744cbd9cf1f71f66ea72
大谷佑香 <ayk.050216@gmail.com>
2020-11-04 10:59:08 +0000

    12章課題実装中

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'],function(){
-  Route::get('news/create','Admin\NewsController@add');
+  Route::get('news/create','Admin\NewsController@add')->middleware('auth');
   //課題4
   Route::get('profile/create','Admin\ProfileController@add');
   Route::get('profile/edit','Admin\ProfileController@edit');
@@ -28,3 +28,7 @@ Route::group(['prefix' => 'admin'],function(){
 
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
