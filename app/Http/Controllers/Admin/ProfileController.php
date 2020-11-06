9c6cf5cb7c41642f8710107f55f476c61fba6718
大谷佑香 <ayk.050216@gmail.com>
2020-11-06 10:52:35 +0000

    カリキュラム14課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace App\Http\Controllers\admin;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class profilecontroller extends Controller
+use App\Profile;
+
+class ProfileController extends Controller
 {
     //
     public function add()
@@ -13,10 +15,7 @@ class profilecontroller extends Controller
       return view('admin.profile.create');
     }
     
-    public function create()
-    {
-      return redirect('admin/profile/create');
-    }
+
     
     public function edit()
     {
@@ -26,5 +25,23 @@ class profilecontroller extends Controller
     public function update()
     {
       return redirect('admin/profile/edit');
+    }
+    
+    public function create(Request $request) 
+    //この文の意味が分からない$requestになにが入っているのか
+    {
+      $this->validate($request, Profile::$rules);  //ここも
+      $profile = new Profile;   //ここも
+      $form = $request->all();   //ここも
+      
+      unset($form['_token']);    
+      //tokenを削除する理由はなんでか
+      //そもそもtokenとは?（フォームから送信された）データ全てのことでしょうか。）
+      unset($form['image']);     //imageもなぜ削除するのか。
+      
+      $profile->fill($form);
+      $profile->save();
+      
+      return redirect('admin/profile/create');
     }
 }
